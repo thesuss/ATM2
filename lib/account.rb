e7fb914c9983c844fe5121325c43df04607d588b
thesuss <lullisuss@gmail.com>
2016-08-09 16:16:01 +0200

    skapat account o spec

---
+++ lib/account.rb
@@ -0,0 +1,5 @@
+class Account
+  #A Ruby constant is like a variable, except that its value is supposed to
+  #remain constant for the duration of the program.
+  STANDARD_VALIDITY_YRS = 5
+end
---
+++ spec/account_spec.rb
@@ -0,0 +1,10 @@
+require './lib/atm.rb'
+require './lib/account.rb'
+
+
+describe Atm do
+it 'is expected to have an expiry date on initialize' do
+    # Here we set the validity of the card to 5 yrs as default
+    expected_date = Date.today.next_year(5).strftime("%m/%y")
+    expect(subject.exp_date).to eq expected_date
+end
