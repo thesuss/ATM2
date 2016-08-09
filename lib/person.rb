fd173cbcd81000312a05c09eaf5c2726604f529d
thesuss <lullisuss@gmail.com>
2016-08-09 17:32:12 +0200

    två persontest gröna

--- lib/person.rb
+++ lib/person.rb
@@ -1,8 +1,19 @@
 class Person
   attr_accessor :name
 
-def initialize (name)
-@name = name
-end
+  def initialize (attrs = {})
+    @name =  set_name(attrs[:name])
+  end
+
+
+  private
+    def set_name(obj)
+    obj == nil ?  missing_name : @name = obj
+  end
+
+  def missing_name
+    raise "A name is required"
+  end
+
 
 end
