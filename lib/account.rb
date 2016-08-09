da3e1ccc9aed625c60b3af7f42c7cc53a76d8758
thesuss <lullisuss@gmail.com>
2016-08-09 16:39:25 +0200

    kontot har ägare

--- lib/account.rb
+++ lib/account.rb
@@ -1,17 +1,33 @@
 class Account
+
+
   #A Ruby constant is like a variable, except that its value is supposed to
   #remain constant for the duration of the program.
   STANDARD_VALIDITY_YRS = 5
   #Man sätter den såhär i stället för inuti koden där den t.ex. kan bli svårhittad ifall den behöver ändras.
-  attr_accessor :exp_date
+  attr_accessor :exp_date, :account_status, :owner
 
   def set_expire_date
-    Date.today.next_year(Account::STANDARD_VALIDITY_YRS).strftime('%m/%y')
+    Date.today.next_year(STANDARD_VALIDITY_YRS).strftime('%m/%y')
   end
 
-  def initialize
+  def initialize(attrs = {})
     #sätter utgångsdatum till ovan när kontot/kortet skapas
     @exp_date = set_expire_date
+    @account_status = :active
+    @owner = set_owner(attrs[:owner])
   end
 
+  def deactivate
+    @account_status = :deactivated
+  end
+
+  private
+    def set_owner(obj)
+    obj == nil ?  missing_owner : @owner = obj
+  end
+
+  def missing_owner
+    raise "An Account owner is required"
+  end
 end
--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -3,9 +3,35 @@ require './lib/account.rb'
 
 
 describe Account do
+
+
+    # we create the double in our `describe` block and give him one sigle attribute
+    let(:person) {instance_double('Person', name: 'DuPont')}
+    # and modify our `subject`
+    subject { described_class.new({owner: person}) }
+
+
   it 'is expected to have an expiry date on initialize' do
     # Here we set the validity of the card to 5 yrs as default
     expected_date = Date.today.next_year(5).strftime("%m/%y")
     expect(subject.exp_date).to eq expected_date
+  end
+
+  it 'is expected to have :active status on initialize' do
+    expect(subject.account_status).to eq :active
+  end
+
+  #instance pga mindre text
+  it 'deactivates account using Instance method' do
+    subject.deactivate
+    expect(subject.account_status).to eq :deactivated
+  end
+
+  it 'is expected to have an owner' do
+    expect(subject.owner).to eq person
+  end
+
+  it 'is expected to raise error if no owner is set' do
+    expect { described_class.new }.to raise_error 'An Account owner is required'
   end
 end
